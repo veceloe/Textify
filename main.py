ad6364ae3f3c8a1d68825d028ea0a16ec971d3f1
chubak <gameproclass@gmail.com>
2023-04-15 00:19:22 +0300

    add summarize

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import aiohttp
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-YOUR_API_KEY = "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"
+YOUR_API_KEY = "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"
 YOUR_API_URL = "https://api.openai.com/v1/chat/completions"
 
 
@@ -23,7 +23,7 @@ async def post_translate(request: Request):
 
     headers = {"Authorization": f"Bearer {YOUR_API_KEY}"}
     data = {
-        "model": "gpt-4",
+        "model": "gpt-3.5-turbo",
         "messages": [{"role": "user", "content": text + f"\ntranslate this text to {language}"}]  # Использование выбранного языка в запросе
     }
 
@@ -34,4 +34,24 @@ async def post_translate(request: Request):
             translated_text = result["choices"][0]["message"]["content"].strip()
 
     return templates.TemplateResponse("index.html", {"request": request, "translated_text": translated_text})
+
+
+@app.post("/summary", response_class=HTMLResponse)
+async def post_summarize(request: Request):
+    form_data = await request.form()
+    text = form_data["text"]
+
+    headers = {"Authorization": f"Bearer {YOUR_API_KEY}"}
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": text + "\nanswer in the russian language, write a summary of the source text by compressing up to 70% of the source text, while retaining the main idea"}]
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(YOUR_API_URL, json=data, headers=headers) as response:
+            result = await response.json()
+            print(result)
+            summarized_text = result["choices"][0]["message"]["content"].strip()
+
+    return templates.TemplateResponse("index.html", {"request": request, "summarized_text": summarized_text})
 
